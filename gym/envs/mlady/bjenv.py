40472447b876e42d3ede637b820aacfe4c18124f
Marc Tremsal <marc.tremsal@gmail.com>
2016-08-20 13:25:58 -0400

    Develop test for blackjack environment

--- gym/envs/mlady/bjenv.py
+++ gym/envs/mlady/bjenv.py
@@ -1,7 +1,9 @@
 import logging, sys
+
 import gym
 from gym import spaces
 from gym.utils import seeding
+
 import numpy as np
 
 def get_hand(deck, player):
@@ -9,60 +11,39 @@ def get_hand(deck, player):
     for i in range(len(deck)):
         if deck[i] == player:
             hand.append(i)
-
     return hand
-
 
 def card_name(n):
     assert type(n) == int
-    assert n >= 0 and n <= 51
-    res = ''.join([['ace',
-      '2',
-      '3',
-      '4',
-      '5',
-      '6',
-      '7',
-      '8',
-      '9',
-      '10',
-      'jack',
-      'queen',
-      'king'][n % 13], ' of ', ['clubs',
-      'diamonds',
-      'hearts',
-      'spades'][n // 13]])
+    assert n>=0 and n<=51
+    res = "".join([["ace","2","3","4","5","6","7","8","9","10","jack","queen","king"][n%13],
+        " of ",
+        ["clubs", "diamonds", "hearts", "spades"][n//13]])
     return res
-
 
 def hand_name(deck, player):
     hand = get_hand(deck, player)
-    cards = [ card_name(c) for c in hand ]
-    return ', '.join(cards)
-
+    cards = [card_name(c) for c in hand]
+    return ", ".join(cards)
 
 def board_name(deck):
     return 'Dealer Hand: {} | Player Hand: {}'.format(hand_name(deck, -1), hand_name(deck, 1))
-
 
 def draw_card(deck, player):
     new_deck = deck
     i = np.random.randint(0, 52)
     while new_deck[i] != 0:
         i = np.random.randint(0, 52)
-
     new_deck[i] = player
     return new_deck
 
-
 def card_value(n):
-    if n % 13 in (10, 11, 12):
+    if n%13 in [10, 11, 12]:
         return 10
-    elif n % 13 == 0:
+    elif n%13 == 0:
         return 11
     else:
-        return n % 13
-
+        return n%13
 
 def generate_deck():
     deck = np.zeros(52)
@@ -71,121 +52,131 @@ def generate_deck():
     deck = draw_card(deck, -1)
     return deck
 
-
 def eval_hand(deck, player):
     hand = get_hand(deck, player)
-    score = sum([ card_value(c) for c in hand ])
-    n_aces = len([ c for c in hand if c % 13 == 0 ])
+    score = sum([card_value(c) for c in hand])
+    n_aces = len([c for c in hand if c%13 == 0])
+
     for i in range(4):
         if score > 21 and n_aces > 0:
             score -= 10
             n_aces -= 1
 
     if score > 21:
-        return (score, 'bust')
+        return score, "bust"
     elif score == 21 and len(hand) == 2:
-        return (21, 'blackjack')
+        return 21, "blackjack"
     elif n_aces > 0:
-        return (score, 'soft')
+        return score, "soft"
     else:
-        return (score, '')
-
+        return score, ""
 
 def provide_actions(deck, player):
     if player == -1:
-        return [0]
+        return [0] # Stand
     else:
         score, qual = eval_hand(deck, 1)
-        if score < 21 and qual != 'bust' and qual != 'blackjack':
+        if score < 21 and qual != "bust" and qual != "blackjack":
             return [0, 1]
-        return [0]
-
+        else:
+            return [0]
 
 def resolve(deck):
     reward = 0
-    info = ''
+    info = ""
     d_score, d_qual = eval_hand(deck, -1)
     p_score, p_qual = eval_hand(deck, 1)
-    if p_qual == 'bust':
+
+    if p_qual == "bust":
         reward = -1
-        info = 'Player busts'
-    elif p_qual == 'blackjack' and d_qual == 'blackjack':
+        info = "Player busts"
+    elif p_qual == "blackjack" and d_qual == "blackjack":
         reward = 0
-        info = 'Player pushes'
-    elif p_qual == 'blackjack':
+        info = "Player pushes"
+    elif p_qual == "blackjack":
         reward = 1.5
-        info = 'Player wins (blackjack)'
-    elif d_qual == 'blackjack':
+        info = "Player wins (blackjack)"
+    elif d_qual == "blackjack":
         reward = -1
-        info = 'Player loses (blackjack)'
-    elif d_qual == 'bust':
+        info = "Player loses (blackjack)"
+    elif d_qual == "bust":
         reward = 1
-        info = 'Player wins'
+        info = "Player wins"
     elif p_score == d_score:
         reward = 0
-        info = 'Player pushes'
+        info = "Player pushes"
     elif p_score > d_score:
         reward = 1
-        info = 'Player wins'
+        info = "Player wins"
     else:
         reward = -1
-        info = 'Player loses'
-    return (reward, info)
+        info = "Player loses"
 
+    return reward, info
 
 class BJEnv(gym.Env):
+
     metadata = {'render.modes': ['human', 'ansi']}
 
     def __init__(self):
+        # Stand == 0, Hit == 1. On bust or after initial stand the agent is forced to stand.
         self.action_space = spaces.Discrete(2)
+        self.allowed_actions = [0,1]
+        # 52 cards with 0 == in the deck, 1 == in player's hand and -1 == in dealer's hand
+        # self.observation_space = spaces.MultiDiscrete([ [0,3] * 52 ])
         self.observation_space = spaces.Box(-1, 1, 52)
         self._seed()
         self._reset()
 
     def _step(self, action):
         assert action in self.action_space
-        info = ''
-        if action == 1:
+        info = ""
+
+        if action == 1: # hit
+            # draw a card
             self.observation = draw_card(self.observation, 1)
             p_score, p_qual = eval_hand(self.observation, 1)
+
             info = board_name(self.observation)
+
             if p_score > 21:
                 done = True
                 reward, info = resolve(self.observation)
             else:
                 done = False
                 reward = 0
-            self.action_space = provide_actions(self.observation, 1)
-        else:
+            self.allowed_actions = provide_actions(self.observation, 1)
+
+        else: # stand
             self.observation = draw_card(self.observation, -1)
+
             p_score, p_qual = eval_hand(self.observation, 1)
             d_score, d_qual = eval_hand(self.observation, -1)
+
             info = board_name(self.observation)
+
             if d_score > 16:
                 done = True
                 reward, info = resolve(self.observation)
             else:
                 done = False
                 reward = 0
-            self.action_space = provide_actions(self.observation, -1)
-        return (self.observation,
-         reward,
-         done,
-         info)
+            self.allowed_actions = provide_actions(self.observation, -1)
+        return self.observation, reward, done, info
 
     def _reset(self):
         self.observation = generate_deck()
         self.action_space = [0, 1]
         return self.observation
 
-    def _render(self, mode = 'human', close = False):
+    def _render(self, mode='human', close=False):
         if close:
             return
         outfile = StringIO() if mode == 'ansi' else sys.stdout
         outfile.write(board_name(self.observation))
-        outfile.write('\n')
+        outfile.write("\n")
         return outfile
 
-    def _seed(self, seed = None):
+    def _seed(self, seed=None):
         self.np_random, seed = seeding.np_random(seed)
         return [seed]
---
+++ gym/envs/tests/test_bjenv.py
@@ -0,0 +1,50 @@
+import nose
+
+import gym
+
+from gym import envs
+from gym.envs.mlady import bjenv
+from gym.envs.mlady.bjenv import eval_hand
+
+import numpy as np
+
+def build_deck(player_hand, dealer_hand):
+    deck = np.zeros(52)
+    for card in player_hand:
+        deck[card] = 1
+    for card in dealer_hand:
+        deck[card] = -1
+    return deck
+
+def setup_func():
+    test_decks = {}
+    test_decks["5"] = build_deck([1,2],[50])
+    test_decks["9"] = build_deck([1,2,3],[50])
+    test_decks["12"] = build_deck([1,10],[50])
+    test_decks["20"] = build_deck([10,11],[50])
+    test_decks["soft 13"] = build_deck([0,1],[50])
+    test_decks["soft 20"] = build_deck([0,8],[50])
+    test_decks["blackjack"] = build_deck([0,10],[50])
+    test_decks["21"] = build_deck([0,10,11],[50])
+    test_decks["bust"] = build_deck([4,6,10],[50])
+    test_decks["bust with 1 ace"] = build_deck([0,4,5,10],[50])
+    test_decks["bust with 2 aces"] = build_deck([0,13,3,5,10],[50])
+    test_decks["hard 21 with 2 aces"] = build_deck([0,13,3,4,10],[50])
+    return test_decks
+
+def eval_hand_test():
+    test_decks = setup_func()
+    print test_decks["5"]
+    print eval_hand(test_decks["5"], 1)
+    assert eval_hand(test_decks["5"], 1) == (5, "")
+    assert eval_hand(test_decks["9"], 1) == (9, "")
+    assert eval_hand(test_decks["12"], 1) == (12, "")
+    assert eval_hand(test_decks["20"], 1) == (20, "")
+    assert eval_hand(test_decks["soft 13"], 1) == (13, "soft")
+    assert eval_hand(test_decks["soft 20"], 1) == (20, "soft")
+    assert eval_hand(test_decks["blackjack"], 1) == (21, "blackjack")
+    assert eval_hand(test_decks["21"], 1) == (21, "")
+    assert eval_hand(test_decks["bust"], 1) == (22, "bust")
+    assert eval_hand(test_decks["bust with 1 ace"], 1) == (22, "bust")
+    assert eval_hand(test_decks["bust with 2 aces"], 1) == (22, "bust")
+    assert eval_hand(test_decks["hard 21 with 2 aces"], 1) == (21, "")
